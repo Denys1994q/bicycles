33b284fa9770866b8eec291c2b2a1ba618f1aa29
Denys1994q <drybachok@gmail.com>
2023-12-26 15:20:28 +0200

    add sass, redux packages, add header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.sass'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <section className='main-container'>
+            <Header />
+                3434
+            <Footer />
+        </section>
+    )
 }
 
 export default App;
